c524ef58d74b91fda5b39d045c3f1fcf28857deb
chinmayakain <chinmaynaik.009@gmail.com>
2023-10-16 22:36:08 +0530

    added factory-method uml and added responsible code

--- src/design-patterns/creational/factory/factory-method/factory/index.ts
+++
@@ -1,5 +0,0 @@
-import { IOrder } from "../../simple-factory/interfaces";
-
-export abstract class FoodOrderFactory {
-    abstract createOrder(item: string, quantity: number, comments?: string): IOrder;
-}
